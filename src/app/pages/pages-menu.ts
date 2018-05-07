633bab345008c81e169d3b9309c98d9d8a6c9b85
Fede Prado <fpradomacat@gmail.com>
2018-05-07 19:31:45 -0300

    cambia el menu, favicon

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -1,166 +1,73 @@
 import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
-  {
-    title: 'Dashboard',
-    icon: 'nb-home',
-    link: '/pages/dashboard',
-    home: true,
-  },
-  {
-    title: 'FEATURES',
-    group: true,
-  },
-  {
-      title: 'Normas',
-      icon: 'nb-tables',
-      children: [
-          {
-              title: 'Consultar',
-              link: '/pages/tables/smart-table',
-          },
-      ],
-  },
-  {
-    title: 'UI Features',
-    icon: 'nb-keypad',
-    link: '/pages/ui-features',
-    children: [
-      {
-        title: 'Buttons',
-        link: '/pages/ui-features/buttons',
-      },
-      {
-        title: 'Grid',
-        link: '/pages/ui-features/grid',
-      },
-      {
-        title: 'Icons',
-        link: '/pages/ui-features/icons',
-      },
-      {
-        title: 'Modals',
-        link: '/pages/ui-features/modals',
-      },
-      {
-        title: 'Popovers',
-        link: '/pages/ui-features/popovers',
-      },
-      {
-        title: 'Typography',
-        link: '/pages/ui-features/typography',
-      },
-      {
-        title: 'Animated Searches',
-        link: '/pages/ui-features/search-fields',
-      },
-      {
-        title: 'Tabs',
-        link: '/pages/ui-features/tabs',
-      },
-    ],
-  },
-  {
-    title: 'Forms',
-    icon: 'nb-compose',
-    children: [
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
-      },
-    ],
-  },
-  {
-    title: 'Components',
-    icon: 'nb-gear',
-    children: [
-      {
-        title: 'Tree',
-        link: '/pages/components/tree',
-      }, {
-        title: 'Notifications',
-        link: '/pages/components/notifications',
-      },
-    ],
-  },
-  {
-    title: 'Maps',
-    icon: 'nb-location',
-    children: [
-      {
-        title: 'Google Maps',
-        link: '/pages/maps/gmaps',
-      },
-      {
-        title: 'Leaflet Maps',
-        link: '/pages/maps/leaflet',
-      },
-      {
-        title: 'Bubble Maps',
-        link: '/pages/maps/bubble',
-      },
-      {
-        title: 'Search Maps',
-        link: '/pages/maps/searchmap',
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    icon: 'nb-bar-chart',
-    children: [
-      {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
-      },
-      {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
-      },
-      {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-    ],
-  },
-  {
-    title: 'Editors',
-    icon: 'nb-title',
-    children: [
-      {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
-      },
-      {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ],
-  },
-  {
-    title: 'Auth',
-    icon: 'nb-locked',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
-  },
+    {
+        title: 'Inicio',
+        icon: 'nb-home',
+        link: '/pages/inicio',
+        home: true,
+    },
+    {
+        title: 'SECCIONES',
+        group: true,
+    },
+    {
+        title: 'Normas',
+        icon: 'nb-compose',
+        children: [
+            {
+                title: 'Consultar',
+                link: '/pages/tables/smart-table',
+            },
+        ],
+    },
+    {
+        title: 'Institutos',
+        icon: 'nb-star',
+        children: [
+            {
+                title: 'ISO',
+                link: '/pages/tables/smart-table1',
+            },
+            {
+                title: 'NIST',
+                link: '/pages/tables/smart-table2',
+            },
+            {
+                title: 'EIA',
+                link: '/pages/tables/smart-table3',
+            },
+            {
+                title: 'IEEE',
+                link: '/pages/tables/smart-table4',
+            },
+            {
+                title: 'MIT',
+                link: '/pages/tables/smart-table5',
+            }
+        ],
+    },
+    {
+        title: 'Auditoria',
+        icon: 'nb-lightbulb',
+        children: [
+            {
+                title: 'Que es?',
+                link: '/pages/tables/smart-table6',
+            },
+            {
+                title: 'Que tipos hay?',
+                link: '/pages/tables/smart-table7',
+            },
+            {
+                title: 'Rol del Auditor',
+                link: '/pages/tables/smart-table8',
+            }
+        ],
+    },
+    {
+        title: 'Contacto',
+        icon: 'nb-person',
+        link: '/pages/tables/smart-table10'
+    }
 ];
