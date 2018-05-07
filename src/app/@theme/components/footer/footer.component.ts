ad8c3417b18b8b6316173ecc9e45d85dfd0616ae
Fede Prado <fpradomacat@gmail.com>
2018-05-07 20:15:59 -0300

    se agrega mail en footer, cambia la pagina de inicio

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
     <span class="created-by">Creado por <b>Federico Prado Macat</b> (2018)</span>
     <div class="socials">
       <a href="https://linkedin.com/in/federico-prado-macat/" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="mailto:fpradomacat@gmail.com" class="ion ion-social-google"></a>
     </div>
   `,
 })
