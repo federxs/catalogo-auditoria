7e755cfe105a15f072f68405f653036dd3e54e2d
Fede Prado <fpradomacat@gmail.com>
2018-06-03 20:30:11 -0300

    Tabla de html y ngfor con link y boton

--- src/app/@core/data/smart-table.service.ts
+++ src/app/@core/data/smart-table.service.ts
@@ -5,36 +5,36 @@ export class SmartTableService {
 
     data = [
         {
-            norma: 'ISO 9001',
+            nombre: 'ISO 9001',
             categoria: 'Calidad',
             descripcion: 'Se puede certificar la 2013',
             link: 'https://es.wikipedia.org/wiki/ISO_9001'
         },
         {
-            norma: 'ISO 9002',
+            nombre: 'ISO 9002',
             categoria: 'Calidad',
             descripcion: 'Se puede certificar la 2015',
             link: 'https://es.wikipedia.org/wiki/ISO_9001'
         },
         {
-            norma: 'ISO 9003',
+            nombre: 'ISO 9003',
             categoria: 'Calidad',
             descripcion: 'Se puede certificar la 2015',
             link: 'https://es.wikipedia.org/wiki/ISO_9001'
         },
         {
-            norma: 'ISO 9004',
+            nombre: 'ISO 9004',
             categoria: 'Calidad',
             descripcion: 'Se puede certificar la 2015',
             link: 'https://es.wikipedia.org/wiki/ISO_9001'
         },
         {
-            norma: 'ISO 9005',
+            nombre: 'ISO 9005',
             categoria: 'Calidad',
             descripcion: 'Se puede certificar la 2015',
             link: 'https://es.wikipedia.org/wiki/ISO_9001'
         },
-        
+
     ];
 
     getData() {
--- src/app/pages/tables/smart-table/smart-table.component.ts
+++ src/app/pages/tables/smart-table/smart-table.component.ts
@@ -1,46 +1,49 @@
-import { Component } from '@angular/core';
-import { LocalDataSource } from 'ng2-smart-table';
+import {Component} from '@angular/core';
+import {LocalDataSource} from 'ng2-smart-table';
 
-import { SmartTableService } from '../../../@core/data/smart-table.service';
+import {SmartTableService} from '../../../@core/data/smart-table.service';
 
 @Component({
-    selector: 'ngx-smart-table',
-    templateUrl: './smart-table.component.html',
-    styles: [`
+  selector: 'ngx-smart-table',
+  templateUrl: './smart-table.component.html',
+  styles: [`
     nb-card {
       transform: translate3d(0, 0, 0);
     }
   `],
 })
 export class SmartTableComponent {
+  let
+  normasVevo = [];
 
-    settings = {
-        actions: false,
-        columns: {
-            norma: {
-                title: 'Norma',
-                type: 'string'
-            },
-            categoria: {
-                title: 'Categoria',
-                type: 'string'
-            },
-            descripcion: {
-                title: 'Descripcion',
-                type: 'string'
-            },
-            link: {
-                title: 'Link',
-                type: 'string'
-            }
-        },
-    };
+  settings = {
+    actions: false,
+    columns: {
+      nombre: {
+        title: 'Norma',
+        type: 'string'
+      },
+      categoria: {
+        title: 'Categoria',
+        type: 'string'
+      },
+      descripcion: {
+        title: 'Descripcion',
+        type: 'string'
+      },
+      link: {
+        title: 'Link',
+        type: 'string'
+      }
+    },
+  };
 
-    source: LocalDataSource = new LocalDataSource();
+  normas: LocalDataSource = new LocalDataSource();
 
-    constructor(private service: SmartTableService) {
-        const data = this.service.getData();
-        this.source.load(data);
-    }
+  constructor(private service: SmartTableService) {
+    const data = this.service.getData();
+    this.normas.load(data);
+    this.normasVevo = data;
+  }
 
 }
